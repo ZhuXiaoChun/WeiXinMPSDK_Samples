fe34a3a77e29eeb80fe0b13cdef94b0166f9f40c
ZhuXiaoChun <zhu.xiaochun@qq.com>
2024-05-13 15:35:00 +0800

    完成了：“公众号（MP）”项目的初步完善和笔记。

---
+++ 公众号（MP）/Senparc.Weixin.Sample.MP/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Microsoft.Extensions.FileProviders;
+global using Senparc.Weixin.AspNet;
+global using Senparc.Weixin.Entities;
+global using Senparc.Weixin.MP;
+global using Senparc.Weixin.MP.MessageHandlers.Middleware;
+global using Senparc.Weixin.RegisterServices;
+global using Senparc.Weixin.Sample.MP;
