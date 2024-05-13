19c7646468e885853e54fad6fea536b7f2edad97
ZhuXiaoChun <zhu.xiaochun@qq.com>
2024-05-13 19:14:22 +0800

    继续：完善“微信支付”项目。

---
+++ 微信支付v3（TenPayV3）/Senparc.Weixin.Sample.TenPayV3/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.Extensions.FileProviders;
+global using Senparc.Weixin.AspNet;
+global using Senparc.Weixin.Entities;
+global using Senparc.Weixin.MP;
+global using Senparc.Weixin.RegisterServices;
+global using Senparc.Weixin.TenPayV3;
---
+++ 微信支付v3（TenPayV3）/Senparc.Weixin.Sample.TenPayV3/Models/ProductModel.cs
@@ -0,0 +1,57 @@
+﻿//DPBMARK_FILE TenPay
+namespace Senparc.Weixin.Sample.TenPayV3.Models;
+
+/// <summary>
+/// 商品实体类
+/// </summary>
+public class ProductModel
+{
+        ////////////////////////////////////////////////
+        // @自身属性
+        ////////////////////////////////////////////////
+
+        #region 自身属性
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+
+        #endregion
+
+
+        ////////////////////////////////////////////////
+        // @自身实现
+        ////////////////////////////////////////////////
+
+        #region 自身实现
+
+        public ProductModel(int id, string name, decimal price)
+        {
+                Id = id;
+                Name = name;
+                Price = price;
+        }
+
+
+        private static List<ProductModel>? ProductList { get; set; }
+
+        public static List<ProductModel> GetFakeProductList()
+        {
+                var list = ProductList ?? new List<ProductModel>()
+    {
+        new ProductModel(1,"产品1",(decimal)0.01),
+        new ProductModel(2,"产品2",(decimal)2.00),
+        new ProductModel(3,"产品3",(decimal)3.00),
+        new ProductModel(4,"产品4",(decimal)4.00),
+        new ProductModel(5,"捐赠1",(decimal)10.00),
+        new ProductModel(6,"捐赠2",(decimal)50.00),
+        new ProductModel(7,"捐赠3",(decimal)100.00),
+        new ProductModel(8,"捐赠4",(decimal)500.00),
+    };
+                ProductList = ProductList ?? list;
+
+                return list;
+        }
+}
+
+#endregion
